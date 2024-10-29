2114441e14e9677323c3186a92dd3a39046b7b89
Yogi Spice <caseynnewman@yahoo.com>
2024-10-29 15:41:08 -0400

    creating generator component, removed extra files

---
+++ src/components/Generator.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './style.css'
+
+export default function Generator() {
+
+    const [generatedFirstName, setGeneratedFirstName] = useState('');
+    const [generatedSecondName, setGeneratedSecondName] = useState('');
+
+    const firstNames = [
+        'glittering', 'sparkling', 'dewey',
+        'forest', 'merriweather'
+    ]
+
+    const secondNames = [
+        'treebeard', 'fawn', 'flora', 'fauna',
+        'raindrops', 'dewdrops'
+    ]
+
+    return (
+        <>
+        
+        </>
+    )
+}
--- src/components/nameList.jsx
+++
@@ -1,7 +0,0 @@
-export const userNames = [
-    "name1",
-    "name2",
-    "name3",
-    "name4",
-    "name5"
-];
