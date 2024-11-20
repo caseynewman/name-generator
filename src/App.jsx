fe24945c30ffbb76cf4d05780c14713b6a332663
Yogi Spice <caseynnewman@yahoo.com>
2024-11-20 09:25:16 -0500

    organizing file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Generator from './components/Generator';
+import Generator from './components/Generator/Generator';
 
 function App() {
 
