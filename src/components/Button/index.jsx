82000749a761516da9f41229ac33375fed1cf594
Yogi Spice <caseynnewman@yahoo.com>
2024-11-21 08:40:12 -0500

    created Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,1 +1,24 @@
 import './style.css';
+import { Link } from 'react-router-dom';
+
+export default function MyButton({ children, href, to, ...props }) {
+    if (to) {
+        return (
+            <Link className='my-button' to={to} {...props}>
+                {children}
+            </Link>
+        );
+    }
+    if (href) {
+        return (
+            <a className='my-button' href={href} {...props}>
+                {children}
+            </a>
+        );
+    }
+    return (
+        <button className='my-button' type='button' {...props}>
+            {children}
+        </button>
+    );
+}
