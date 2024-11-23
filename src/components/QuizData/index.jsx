5bf8f0aba4a92ede86f9f641c01e3c6abb83f9ab
Yogi Spice <caseynnewman@yahoo.com>
2024-11-22 22:46:26 -0500

    created quizData file with array of objects for questions and options

---
+++ src/components/QuizData/index.jsx
@@ -0,0 +1,11 @@
+export const quizData = [
+    {
+        id: 1,
+        text: "You're at the edge of a forest...",
+        options: [
+            { text: "You go in.", nextQuestion: 2 },
+            { text: "You head toward the meadow.", nextQuestion: 3 },
+        ],
+    },
+    //more questions
+];
