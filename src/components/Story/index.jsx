776a4e4ca1a60ceed48ae06fdf62b6520a571500
Yogi Spice <caseynnewman@yahoo.com>
2024-11-24 23:01:26 -0500

    created Story component

---
+++ src/components/Story/index.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Story({ storyData }) {
+    const [currentScene, setCurrentScene] = useState('start');
+
+    const handleOptionClick = (nextScene) => {
+        setCurrentScene(nextScene);
+    };
+
+    const scene = storyData[currentScene];
+
+    return (
+        <div>
+            <p>{scene.text}</p>
+            {scene.option.man((option, index) => (
+                <Option key={index} option={option} onClick={handleOptionClick} />
+            ))}
+        </div>
+    )
+}
