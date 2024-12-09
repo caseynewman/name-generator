693d3aefb04cbda799db6aa6f39b6d4251d0658d
Yogi Spice <caseynnewman@yahoo.com>
2024-12-08 22:17:55 -0500

    added intro blurb to GameStart component

--- src/pages/GameStart/index.jsx
+++ src/pages/GameStart/index.jsx
@@ -6,7 +6,7 @@ export default function GameStart() {
     return (
         <div className='game-container'>
             <p className='game-intro'>
-                Imagine...
+                Imagine living amongst nature. When you envision your dream scenario, are there lush trees or bubbling brooks or vast cliffsides? Take a walk to find yourself. Uncover where you would call home, and generate a random name at the end of your adventure based on your answers along the way.
             </p>
             <MyButton>
                 Let's go!
