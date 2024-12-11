722b22cb3e714cd56e654914bf11dd1b7f0ae461
Yogi Spice <caseynnewman@yahoo.com>
2024-12-10 23:22:15 -0500

    re-wording intro blurb

--- src/pages/GameStart/index.jsx
+++ src/pages/GameStart/index.jsx
@@ -6,7 +6,8 @@ export default function GameStart() {
     return (
         <div className='game-container'>
             <p className='game-intro'>
-                Imagine living amongst nature. When you envision your dream scenario, are there lush trees or bubbling brooks or vast cliffsides? Take a walk to find yourself. Uncover where you would call home, and generate a random name at the end of your adventure based on your answers along the way.
+                Imagine living amongst nature. When you envision your dream home, are there lush trees or bubbling brooks or vast cliffsides?<br/><br/>
+                Take a walk to find yourself. When you've uncovered the place where you would call home, generate a random name at the end of your adventure based on your answers along the way.
             </p>
             <MyButton>
                 Let's go!
